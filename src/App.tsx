cbed3355fa377fc1c202031e1db36043cb1ab15b
TatsianaYurchik <t26yurchik@gmail.com>
2023-01-04 20:06:02 +0300

    add custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,15 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Header } from './components/Header';
 import { Product } from './components/Product';
 import { products } from './data/products';
-import { Row,Col } from 'react-bootstrap';
-import axios, { AxiosError } from 'axios';
-import { IProduct } from './models';
-import { useState } from 'react';
+import { Row} from 'react-bootstrap';
+import { useProduct } from './hooks/fetchProduct';
 
 
 function App() {
-  const[products,setProducts]=useState<IProduct[]>([])
-  const [loading,setLoading]= useState(false)
-  const [error,setError]= useState('')
 
-async function fetchProducts() {
-  try {
-  setError("")
-  setLoading(true)
-  const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?')
-  setProducts(response.data)
-  setLoading(false)
-  } catch (e:unknown) {
-    const error = e as AxiosError
-    setLoading(false)
-    setError(error.message)
-  }
-
-
-  
-
-}
-
-useEffect(()=>{
- fetchProducts()
-},[])
-
+const {loading,products,error}=useProduct()
   return (
     <Container>
     <Header/>
